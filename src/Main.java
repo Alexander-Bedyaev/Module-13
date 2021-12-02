a9f34f3dfe72f75d9ff13b6a8466d94e27b6149a
Alexander Bedyaev <hairedball@gmail.com>
2021-12-03 01:58:08 +0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Operator operator1 = new Operator();
+        Operator operator2 = new Operator();
+        operator1.start();
+        operator2.start();
+
+
+
 
 
     }
--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,46 +1,43 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+
 import java.util.Random;
 
 public class PhoneBook {
     private static final String[] nameArray = new String[]{"Петр", "Максим", "Станислав", "Клим", "Александр"};
-    private static final String[] namePatArray = new String[nameArray.length];
-    private static final String[] phoneNumberArray = new String[nameArray.length];
+    public String[] namePatArray;
+    public String[] phoneNumberArray;
 
-    private void getNamePatArray() {
+    PhoneBook() {
+        namePatArray = new String[nameArray.length];
+        namePatArray = getNamePatArray();
+        phoneNumberArray = new String[nameArray.length];
+        phoneNumberArray = getPhoneNumbers();
+    }
+
+    private String[] getNamePatArray() {
         String[] patronymicArray = new String[nameArray.length];
         Random random = new Random();
         String ovich = "ович";
+
         for (int i = 0; i < patronymicArray.length; i++) {
             patronymicArray[i] = nameArray[i] + ovich;
         }
-        for (int i = 0; i < namePatArray.length; i++) {
-            int j = random.nextInt(namePatArray.length - 1);
+        for (int i = 0; i < nameArray.length; i++) {
+            int j = random.nextInt(nameArray.length - 1);
             namePatArray[i] = nameArray[i] + " " + patronymicArray[j];
         }
-
+        return namePatArray;
     }
 
-    private void getPhoneNumbers() {
+    private String[] getPhoneNumbers() {
         Random random = new Random();
 
         for (int i = 0; i < phoneNumberArray.length; i++) {
             phoneNumberArray[i] = "+7(" + (random.nextInt(99) + 900) + ")" +
                     String.format("%07d", random.nextInt(9999999));
         }
-
+        return phoneNumberArray;
     }
 
-    public void getPhoneBook() {
-        getPhoneNumbers();
-        getNamePatArray();
-        HashMap<String, String> phoneBook = new HashMap<String, String>();
-        for (int i = 0; i < phoneNumberArray.length; i++) {
-            phoneBook.put(phoneNumberArray[i], namePatArray[i]);
-        }
-
-    }
 
 }
 
